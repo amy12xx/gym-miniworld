174534904bd0a4574f727451b0c2ff4002adbb03
Amanda Dsouza <meezamanda@yahoo.com>
2022-03-16 20:13:00 -0700

    gym version setup 1.17.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     keywords='environment, agent, rl, openaigym, openai-gym, gym, robotics, 3d',
     packages=['gym_miniworld', 'gym_miniworld.envs'],
     install_requires=[
-        'gym>=0.9.0',
+        'gym>=0.9.0,<=0.17.3',
         'numpy>=1.10.0',
         'pyglet>=1.5.11',
     ],
