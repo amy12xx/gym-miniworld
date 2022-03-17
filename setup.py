cd6356deeb88304a9eb44377e586b29d4da2bb24
Amanda Dsouza <meezamanda@yahoo.com>
2022-03-16 20:15:49 -0700

    no install gym, numpy

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     keywords='environment, agent, rl, openaigym, openai-gym, gym, robotics, 3d',
     packages=['gym_miniworld', 'gym_miniworld.envs'],
     install_requires=[
-        'gym>=0.9.0,<=0.17.3',
-        'numpy>=1.10.0',
+        # 'gym>=0.9.0,<=0.17.3',
+        # 'numpy>=1.10.0',
         'pyglet>=1.5.11',
     ],
     # Include textures and meshes in the package
